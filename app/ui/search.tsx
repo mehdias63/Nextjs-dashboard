957de11409c5b1c5eddfa0b892e5f7ef6c0f36ee
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-09-14 16:27:09 +0330

    add Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,7 +16,9 @@ export default function Search({
 	const pathname = usePathname()
 	const { replace } = useRouter()
 	function handleSearch(term: string) {
+		console.log(`Searching... ${term}`)
 		const params = new URLSearchParams(searchParams)
+		params.set('page', '1')
 		if (term) {
 			params.set('query', term)
 		} else {
@@ -35,6 +37,7 @@ export default function Search({
 				onChange={e => {
 					handleSearch(e.target.value)
 				}}
+				defaultValue={searchParams.get('query')?.toString()}
 			/>
 			<MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 		</div>
