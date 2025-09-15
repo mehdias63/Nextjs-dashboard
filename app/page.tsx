5e86a704511d92ca8bf18b977483a2e2667dce77
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-09-15 19:38:31 +0330

    add action

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo'
-import { ArrowRightIcon } from '@heroicons/react/24/outline'
+// import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import Image from 'next/image'
 
@@ -29,7 +29,7 @@ export default function Page() {
 						className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
 					>
 						<span>Log in</span>{' '}
-						<ArrowRightIcon className="w-5 md:w-6" />
+						{/* <ArrowRightIcon className="w-5 md:w-6" /> */}
 					</Link>
 				</div>
 				<div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
